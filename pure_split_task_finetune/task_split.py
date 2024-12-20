f998473f9d47e2c78cd22dc304123f9773e6951e
aggar368 <bnm9616700@yahoo.com.tw>
2024-12-21 00:37:59 +0800

    Add split task inference code

---
+++ pure_split_task_finetune/task_split.py
@@ -0,0 +1,43 @@
+import os
+import json
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_folder", type=str, default="/content/dataset/train/")
+    parser.add_argument("--json_path", type=str, default="/content/dataset/train.json")
+
+    args = parser.parse_args()
+
+    task_types = ["general", "regional", "suggestion"]
+    lst_general, lst_regional, lst_suggestion = [], [], []
+    for task in task_types:
+        os.makedirs(os.path.join(args.image_folder, task), exist_ok=True)
+
+    with open(args.json_path, "r") as f:
+        data_lst = json.load(f)
+
+    for data in data_lst:
+        data_type = data["id"].split("_")[1]
+        new_image_path = os.path.join(args.image_folder, data_type, data["id"]+".png")
+        # move image file
+        os.rename(data["image"], new_image_path)
+        # modify image path in data
+        data["image"] = new_image_path
+        # add to new jsons
+        if data_type == "general":
+            lst_general.append(data)
+        elif data_type == "regional":
+            lst_regional.append(data)
+        elif data_type == "suggestion":
+            lst_suggestion.append(data)
+
+    with open(os.path.join(args.image_folder, "general.json"), "w") as f:
+        json.dump(lst_general, f, indent=4)
+    with open(os.path.join(args.image_folder, "regional.json"), "w") as f:
+        json.dump(lst_regional, f, indent=4)
+    with open(os.path.join(args.image_folder, "suggestion.json"), "w") as f:
+        json.dump(lst_suggestion, f, indent=4)
+
+
+    
