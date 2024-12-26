ff116a6b6fe59c184f3d8493c3146f2852539406
aoyis <aoyi2000@gmail.com>
2024-12-26 14:59:24 +0000

    Update bash

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Google drive link: 'https://drive.google.com/drive/folders/1B0zK2pS6WClY2Q9_wR4W4DNF2t5u0MaS?usp=sharing'
-gdown -O training_json --folder 'https://drive.google.com/drive/folders/1rUHfGLs3H7s1ssF4QgDvyDh_nVUDA-N4?usp=sharing'
+gdown -O train_json --folder 'https://drive.google.com/drive/folders/1rUHfGLs3H7s1ssF4QgDvyDh_nVUDA-N4?usp=sharing'
+gdown -O test_json --folder 'https://drive.google.com/drive/folders/1Ho7Fjorfdh2l_J5KHP7Y48ER1Mmd9psX?usp=sharing'
 gdown -O dataset_concat.zip 'https://drive.google.com/uc?id=1IAqdffNCDbWjUS_zGXScnwDN-UxrQxHn'
