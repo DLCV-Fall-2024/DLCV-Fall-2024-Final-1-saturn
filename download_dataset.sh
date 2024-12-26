7cb1ed858ae4db9101e53892a5c8591f554197b2
aoyis <aoyi2000@gmail.com>
2024-12-26 13:26:30 +0000

    Add download dataset bash script

---
+++ download_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Google drive link: 'https://drive.google.com/drive/folders/1B0zK2pS6WClY2Q9_wR4W4DNF2t5u0MaS?usp=sharing'
+gdown -O training_json --folder 'https://drive.google.com/drive/folders/1rUHfGLs3H7s1ssF4QgDvyDh_nVUDA-N4?usp=sharing'
+gdown -O dataset_concat.zip 'https://drive.google.com/uc?id=1IAqdffNCDbWjUS_zGXScnwDN-UxrQxHn'
