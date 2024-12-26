8b331a2df97070d4ecb04b3964f2e211da8793fb
Kevin-114 <77486347+Kevin-114@users.noreply.github.com>
2024-12-26 22:29:43 +0800

    add unzip

--- download_ckpt.sh
+++ download_ckpt.sh
@@ -4,3 +4,8 @@
 gdown -O llava-v1.5-7b-task-lora-general.zip 'https://drive.google.com/uc?id=1DBu9kVcwA2uCD3Tq2w51CdiR7nmedM9G'
 gdown -O llava-v1.5-7b-task-lora-regional.zip 'https://drive.google.com/uc?id=1ItBK2ItZCjkznFiBTpDNIcDeFpN6Rhx8'
 gdown -O llava-v1.5-7b-task-lora-suggestion.zip 'https://drive.google.com/uc?id=1mMTczDtgTfNP8o_psInY_2U4XqIIwfXx'
+
+
+unzip llava-v1.5-7b-task-lora-general.zip
+unzip llava-v1.5-7b-task-lora-regional.zip
+unzip llava-v1.5-7b-task-lora-suggestion.zip
